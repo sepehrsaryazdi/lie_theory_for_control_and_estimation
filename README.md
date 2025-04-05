d970a03cad7730d0eadd9d88ca61c518e8dc33d4
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2025-04-06 09:10:03 +1000

    Saved current progress

---
+++ README.md
@@ -0,0 +1,29 @@
+# Algebra, Geometry and Robots Talk Slides
+
+This repository will run the interactive and dynamic talk titled "Algebra, Geometry and Robots" as presented by Sepehr Saryazdi at the University of Sydney Mathematics Society (SUMS). 
+
+A recording of the presentation is available [here](https://drive.google.com/file/d/1TEoP5Aoc57DKB4ptKwzoWVb-TJAKnSIO/view?usp=sharing). There were some typos in the slides during the presentation, which are fixed in this version. Some corrections about the history of Kempe's Universality Theorem in the presentation: there isn't evidence that Thurston realised the incorrectness of Kempe's construction or wrote down the corollary stated, although he did state the corollary without formal proof during one of his talks. See [here](https://arxiv.org/pdf/math/9807023) for more details.
+
+To run backend server, run the following from this folder:
+
+```
+python3 localServer/server.py
+```
+
+Open `index.html` for the slides. This is what should appear on the HTML page. Use left/right arrow keys to navigate between slides.
+
+<p align="center">
+<img src="readme_images/title_page.png" height="600">
+</p>
+
+
+If the HTML page is working correctly, interactive components should appear:
+
+
+<p align="center">
+<img src="readme_images/interactive_component.png" height="600">
+</p>
+
+
+
+
---
+++ src/scripts/backend.js
@@ -0,0 +1,139 @@
+// simple.js
+
+
+
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+    'src/scripts/pdfjs-3.3.122-dist/build/pdf.worker.js';
+
+// var vid = document.getElementById("steamEngineVideo");
+// vid.playbackRate = 10;
+
+
+// var vid = document.getElementById("roboticArmVideo");
+// vid.playbackRate = 1;
+
+
+var dummyPdf = "http://localhost:8080"
+
+var loadingTask = pdfjsLib.getDocument(dummyPdf);
+
+var pdfSections = document.getElementsByClassName("pdfSection");
+
+loadingTask.promise.then(
+	function (pdf) {
+		// Load information from the first page.
+		for(i = 0 ; i < pdfSections.length; i++){
+			// console.log(pdf.getPage(2))
+			pdf.getPage(i+1).then(function (page) {
+				var scale = 10;
+				var viewport = page.getViewport({scale: scale});
+				var canvas = pdfSections[page._pageIndex];
+				// console.log(canvas)
+
+				var context = canvas.getContext("2d");
+				// console.log(viewport);
+
+				canvas.height = viewport.height;
+				canvas.width = viewport.width;
+
+			//    canvas.height = 0.5*window.innerHeight;
+			//    canvas.width = 0.5*window.innerWidth;
+				
+				// Render the page into the `<canvas>` element.
+				var renderContext = {
+					canvasContext: context,
+					viewport: viewport,
+				};
+			
+				page.render(renderContext);
+
+			
+        
+
+			// page.render(renderContext);
+            
+            // .then(function () {
+			// 	console.log('Page rendered!');
+			// });
+			
+			});
+		
+		}
+	},
+	function (reason) {
+		console.error(reason);
+	},
+);
+
+
+// class ScrollController() {
+// 	computeDivTopAbsolutePosition(div) {
+//         return div.getBoundingClientRect().top + window.pageYOffset
+//     }
+
+//     computeDivBottomAbsolutePosition(div) {
+//         return div.getBoundingClientRect().bottom + window.pageYOffset
+//     }
+
+//     computeDivAbsoluteDifference(div1,div2) {
+//         return this.computeDivTopAbsolutePosition(div2) - this.computeDivBottomAbsolutePosition(div1)
+//     }
+// }
+
+
+function computeDivTopAbsolutePosition(div) {
+	return div.getBoundingClientRect().top + window.pageYOffset
+}
+
+pdfTopPositions = []
+for(i=0 ; i < pdfSections.length ; i++){
+	pdfTopPositions.push(computeDivTopAbsolutePosition(pdfSections[i]));
+}
+
+console.log(pdfTopPositions);
+
+// window.addEventListener('scroll', function(event) {
+// 	let scroll = this.scrollY;
+// 	// console.log(computeDivTopAbsolutePosition(pdfSections[0]));
+// 	console.log(scroll);
+// 	for(i = 0 ; i < pdfSections.length-1 ; i++){
+// 		if(scroll > pdfTopPositions[i]) {
+// 			pdfSections[i+1].scrollIntoView();
+// 		}
+// 	}
+// })
+
+var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+var currentSection = 0;
+
+console.log(currentScroll);
+
+for(i = 0 ; i < pdfSections.length-1 ; i++){
+	if(currentScroll >= pdfTopPositions[i] & currentScroll < pdfTopPositions[i+1]){
+		currentSection = i;
+	}
+}
+
+
+
+document.addEventListener('keydown', event => {
+	event.preventDefault();
+	if (event.code === 'Space' || event.code === "Enter" || event.code == "ArrowDown" || event.code == "ArrowRight") {
+	  console.log('Space pressed')
+	  currentSection += 1
+	  if(currentSection >= pdfSections.length){
+		currentSection = pdfSections.length - 1;
+	  }
+	  
+	} else if (event.code == "ArrowUp" || event.code == "ArrowLeft") {
+		currentSection -= 1
+		if(currentSection < 0){
+			currentSection = 0;
+		}
+	}
+
+	console.log(currentSection)
+
+
+	pdfSections[currentSection].scrollIntoView();
+  })
